6f1d2b1866e2a7aaf85bd19596ccfc42c1b4061d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-17 15:00:24 -0400

    Write if statement

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 let age = 20;
+if (age < 15)
+    age += 10;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 let age: number = 20;
+if (age < 15)
+    age += 10;
