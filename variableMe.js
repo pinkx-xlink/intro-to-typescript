6ff02dc9bd213e5c4f17cabcc63000a46e85c7df
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-17 16:20:06 -0400

    Practice with boolean, number, string, and any types

--- variableMe.js
+++ variableMe.js
@@ -1,2 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello, Shy!";
+greetings.toLowerCase();
 console.log(greetings);
+// number
+var userId = 334455;
+userId.toFixed();
+// boolean
+var isLoggedIn = false;
--- variableMe.ts
+++ variableMe.ts
@@ -1,9 +1,24 @@
 let greetings: string = "Hello, Shy!"
 
-let mynum = 5;
-
 greetings.toLowerCase();
-
 console.log(greetings);
+
+// number
+let userId = 334455
+userId.toFixed();
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// any
+
+let hero: string;
+
+function getHero() {
+    return "thor"
+}
+
+hero = getHero();
+
 
 export {}
